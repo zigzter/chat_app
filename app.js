9dda7582ea38e9f325236b094459ece5a9241075
Ziggy Shea <zigs_17@hotmail.com>
2018-09-20 15:24:42 -0700

    Chat functionality working with auto scroll and 'is typing' message. Replaced custom css with bootstrap

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const logger = require('morgan');
+const xssFilters = require('xss-filters');
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -7,7 +8,7 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 app.get('/', (req,res) => {
-    res.send("ELLO GUVNA");
+    res.render('index');
 });
 
 server = app.listen(3000);
@@ -16,4 +17,19 @@ const io = require('socket.io')(server);
 
 io.on('connection', socket => {
     console.log('New user connected');
+    socket.username = 'Anonymous';
+    socket.on('change_username', data => {
+        socket.username = xssFilters.inHTMLData(data.username);
+    });
+    socket.on('new_message', data => {
+        let message = xssFilters.inHTMLData(data.message);
+        io.sockets.emit('new_message', { message, username: socket.username })
+    });
+    socket.on('typing', data => {
+        socket.broadcast.emit('typing', { username: socket.username })
+    });
+
+    socket.on('clear_typing', data => {
+        socket.broadcast.emit('clear_typing');
+    });
 });
---
+++ public/chat.js
@@ -0,0 +1,52 @@
+$(function(){
+    let socket = io.connect('http://localhost:3000');
+
+    let message = $('#message');
+    let username = $('#username');
+    let send_message = $('#send_message');
+    let send_username = $('#send_username');
+    let chatroom = $('#chatroom');
+    let feedback = $('#feedback p i');
+
+    function scroll(){
+        $('#chatroom').scrollTop($('#chatroom')[0].scrollHeight);
+    }
+
+    $(document).keypress(function(event){
+        let keycode = (event.keyCode ? event.keyCode : event.which);
+        if(keycode == '13'){
+            send_message.click();   
+        }
+    });
+
+    send_message.click(() => {
+        socket.emit('new_message', { message: message.val() });
+        socket.emit('clear_typing');
+        message.val('');
+    });
+    
+    socket.on('new_message', data => {
+        chatroom.append(`<p class='message'> ${data.username} : ${data.message}</p>`);
+        $('html, body').animate({ scrollTop: $('.message').last().offset().top }, 300);
+    });
+
+    send_username.click(() => {
+        socket.emit('change_username', { username: username.val() })
+    });
+
+    message.bind('input', () => {
+        if(message.val()){
+            socket.emit('typing');
+        } else {
+            socket.emit('clear_typing');
+        }
+    });
+
+    socket.on('typing', data => {
+        feedback.text(`${data.username} is typing a message...`)
+    });
+
+    socket.on('clear_typing', data => {
+        feedback.text("");
+    });
+});
